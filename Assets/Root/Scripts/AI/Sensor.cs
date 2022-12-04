9d5e38a12a25bea962a1902f91211a401a384609
Alexi Khailtuud <khai.ilch167@gmail.com>
2022-12-03 20:48:30 -0500

    Created Trigger parent class

--- Assets/Root/Scripts/AI/Sensor.cs
+++ Assets/Root/Scripts/AI/Sensor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trigger : MonoBehaviour
+public class Sensor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Root/Scripts/AI/Trigger.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+   //reference to the TriggerSystemManager
+   protected TriggerSystemManager manager;
+   //Trigger position
+   protected Vector3 position;
+   //Trigger radius
+   public int radius;
+   public bool toBeRemoved;
+
+   private void Awake()
+   {
+      manager = FindObjectOfType<TriggerSystemManager>();
+   }
+
+   public virtual void TryDetection(Sensor sensor)
+   {
+         
+   }
+
+   public virtual void UpdateStatus()
+   {
+      
+   }
+
+   protected virtual bool isTrigger(Sensor sensor)
+   {
+      return false;
+   }
+   
+   
+}
