8644c53585698e5209248269c198ddb171c79f3f
Alexi Khailtuud <khai.ilch167@gmail.com>
2022-12-07 00:54:22 -0500

    Created SenseMemory class

--- Assets/Root/Scripts/AI/SenseMemory.cs
+++ Assets/Root/Scripts/AI/SenseMemory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,33 @@ public class SenseMemory : MonoBehaviour
     public float memoryTime = 4.0f;
     public List<MemoryItem> memoryList = new List<MemoryItem>();
     private List<MemoryItem> removeList = new List<MemoryItem>();
+
+    private void Update()
+    {
+        removeList.Clear();
+
+        foreach (MemoryItem memo in memoryList)
+        {
+            memo.memoryTimeLeft -= Time.deltaTime;
+            
+            if (memo.memoryTimeLeft < 0)
+            {
+                removeList.Add(memo);
+            }
+            else
+            {
+                if (memo.go != null)
+                {
+                    Debug.DrawLine(transform.position, memo.go.transform.position, Color.blue);
+                }
+            }
+        }
+        
+        foreach (MemoryItem memo in removeList)
+        {
+            memoryList.Remove(memo);
+        }
+    }
 
     public bool FindInList()
     {
@@ -30,7 +58,18 @@ public class SenseMemory : MonoBehaviour
             {
                 alreadyInList = true;
                 memo.lastMemoryTime = Time.time;
+                if (type > memo.sensorType)
+                {
+                    memo.sensorType = type;
+                }
+                break;
             }
+        }
+
+        if (!alreadyInList)
+        {
+            MemoryItem newItem = new MemoryItem(go, Time.time, memoryTime, type);
+            memoryList.Add(newItem);
         }
     }
 }
