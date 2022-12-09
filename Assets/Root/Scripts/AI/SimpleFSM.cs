968c4c6ab22d714b002d2ea0b349d546edbebaac
Alexi Khailtuud <khai.ilch167@gmail.com>
2022-12-09 01:23:17 -0500

    Implemented AI Chase & Attack state

--- Assets/Root/Scripts/AI/SimpleFSM.cs
+++ Assets/Root/Scripts/AI/SimpleFSM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.PlayerLoop;
+using UnityEngine.Serialization;
 using Random = System.Random;
 
 public class SimpleFSM : MonoBehaviour
@@ -11,13 +12,23 @@ public class SimpleFSM : MonoBehaviour
     private NavMeshAgent navMeshAgent;
     
     public FSMState curState;
+    
     public bool isDead;
     
     private GameObject[] patrolPointList;
     private Vector3 destPos;
     public float arriveDistance = 2f;
 
-    private int curIndex = 0;
+    private Transform playerTransform;
+    private float chaseDistance = 15f;
+    private float attackDistance = 5f;
+
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawnPoint;
+
+    private float fireRate;
+    private float elapsedTime;
+    
     private void Start()
     {
         Init();
@@ -36,12 +47,15 @@ public class SimpleFSM : MonoBehaviour
                 UpdatePatrolState();
                 break;
             case FSMState.CHASE:
+                UpdateChaseState();
                 break;
             case FSMState.ATTACK:
+                UpdateAttackState();
                 break;
             case FSMState.DIE:
                 break;
         }
+        elapsedTime += Time.deltaTime;
     }
     
     private void Init()
@@ -51,39 +65,96 @@ public class SimpleFSM : MonoBehaviour
         
         curState = FSMState.PATROL;
         isDead = false;
+        elapsedTime = 0f;
+        fireRate = 0.35f;
         patrolPointList = GameObject.FindGameObjectsWithTag("PatrolPoints");
         
         FindNextPoint();
+        
+        GameObject objPlayer = GameObject.FindWithTag("Player");
+        playerTransform = objPlayer.transform;
     }
 
     private void UpdatePatrolState()
     {
-        Debug.Log("lengnth: "+ Vector3.Distance(transform.position, destPos));
+        //Debug.Log("length: "+ Vector3.Distance(transform.position, destPos));
         
         if (Vector3.Distance(transform.position, destPos) <= arriveDistance)
         {
             FindNextPoint();
         }
-
-        //Vector3 v = new Vector3(1, 1, 1);
-        //Vector3 v1 = Quaternion.Euler(0, 90, 0) * v;
+        else if (Vector3.Distance(transform.position, playerTransform.position) <= chaseDistance)
+        {
+            Debug.Log("entering chase");
+            curState = FSMState.CHASE;
+        }
+    }
+    
+    private void UpdateChaseState()
+    {
+        destPos = playerTransform.position;
+        navMeshAgent.SetDestination(destPos);
         
-        //Quaternion target = Quaternion.LookRotation(destPos - transform.position);
-        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 5f);
+        float dist = Vector3.Distance(transform.position, playerTransform.position);
+
+        if (dist <= attackDistance)
+        {
+            Debug.Log("Entering attack state");
+            curState = FSMState.ATTACK;
+        }
+        else if (dist >= chaseDistance)
+        {
+            Debug.Log("Back to patrol");
+            FindNextPoint();
+            curState = FSMState.PATROL;
+        }
+    }
+
+    public void UpdateAttackState()
+    {
+        destPos = playerTransform.position;
+        navMeshAgent.SetDestination(destPos);
+        
+        float dist = Vector3.Distance(transform.position, playerTransform.position);
+
+        if (dist >= attackDistance)
+        {
+            curState = FSMState.CHASE;
+            return;
+        }
+        
+        ShootBullet();
     }
 
     private void FindNextPoint()
     {
-        Debug.Log("FindingNextDes");
+        //Debug.Log("FindingNextDes");
         int randIndex = UnityEngine.Random.Range(0, patrolPointList.Length);
-        
-        // int nextIndex = curIndex + 1;
-        // if (nextIndex++ >= patrolPointList.Length)
-        // {
-        //     nextIndex = 0;
-        // }
-        
+
         destPos = patrolPointList[randIndex].transform.position;
         navMeshAgent.SetDestination(destPos);
+    }
+
+    private void ShootBullet()
+    {
+        if (elapsedTime >= fireRate)
+        {
+            GameObject bulletObj = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, transform.rotation);
+            elapsedTime = 0;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, destPos);
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, arriveDistance);
+        
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
     }
 }
